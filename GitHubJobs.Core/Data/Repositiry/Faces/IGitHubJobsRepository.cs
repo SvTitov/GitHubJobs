f9058a202c31b9203dd6ef11908d5c88d811ddd7
Svyatoslav Titov <slava.titoff@yandex.ru>
2018-07-02 21:11:17 +0400

    Edit repository

--- GitHubJobs.Core/Data/Repositiry/Faces/IGitHubJobsRepository.cs
+++ GitHubJobs.Core/Data/Repositiry/Faces/IGitHubJobsRepository.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using GitHubJobs.Core.Data.Models.Incoming;
+using GitHubJobs.Core.Data.Models.Outgoing;
 
 namespace GitHubJobs.Core.Data.Repositiry.Faces
 {
 	public interface IGitHubJobsRepository 
 	{
-	
+        Task<IEnumerable<JobDescription>> GetAllJobs();
+        Task<IEnumerable<JobDescription>> GetJobs(JobRequest request);
 	}
 }
--- GitHubJobs.Core/Data/Repositiry/Impl/GitHubJobsRepository.cs
+++ GitHubJobs.Core/Data/Repositiry/Impl/GitHubJobsRepository.cs
@@ -4,16 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using GitHubJobs.Core.Data.Models;
+using GitHubJobs.Core.Data.Models.Incoming;
+using GitHubJobs.Core.Data.Models.Outgoing;
+using System.Threading.Tasks;
 
 namespace GitHubJobs.Core.Data.Repositiry.Impl
 {
 	public class GitHubJobsRepository : IGitHubJobsRepository
     {
+        RestWrapper _restWrapper;
+
         public string MainUrl => @"https://jobs.github.com/positions.json";
 
-        //public IEnumerable<JobDescription> GetAllJobs()
-        //{
+        public GitHubJobsRepository()
+        {
+            _restWrapper = new RestWrapper(MainUrl);
+        }
 
-        //}
+        public async Task<IEnumerable<JobDescription>> GetAllJobs()
+        {
+            var result = await _restWrapper.ExecuteGet<List<JobDescription>, JobRequest>();
+            return result;
+        }
+
+        public async Task<IEnumerable<JobDescription>> GetJobs(JobRequest request)
+        {
+            var result = await _restWrapper.ExecuteGet<List<JobDescription>, JobRequest>(request);
+            return result;
+        }
     }
 }
