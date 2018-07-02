09918ad09009fdf05463787b6cde63ac00f201cc
Svyatoslav Titov <slava.titoff@yandex.ru>
2018-07-02 21:43:37 +0400

    Add main view model

---
+++ GitHubJobs.Core/Views/MainScreenViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reactive;
+using GitHubJobs.Core.Data.Models.Incoming;
+using GitHubJobs.Core.Data.Repositiry.Faces;
+using System.Reactive.Linq;
+using System.Collections.Generic;
+
+namespace GitHubJobs.Core.Views
+{
+    public class MainScreenViewModel
+    {
+        public IObservable<JobDescription> Jobs { get; set; }
+
+        public async void GetJobs()
+        {
+            IGitHubJobsRepository repository = null;
+            IEnumerable<JobDescription> result = await repository.GetAllJobs();
+            IObservable<JobDescription> casted = result.ToObservable();
+
+            Jobs = casted;
+        }
+    }
+}
--- GitHubJobs/MainActivity.cs
+++ GitHubJobs/MainActivity.cs
@@ -2,18 +2,23 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using GitHubJobs.Core.Views;
 
 namespace GitHubJobs
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        MainScreenViewModel mainScreenViewModel = new MainScreenViewModel();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            //mainScreenViewModel.Jobs.Su
         }
     }
 }
